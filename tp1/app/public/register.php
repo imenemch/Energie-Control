d0f42efe7c7aa54864cdcd89d80ef6b48fb2f99b
imene.maouche <imene.maouche@rtit-consulting.com>
2024-02-24 11:48:16 +0100

    debug probleme register

--- tp1/app/public/register.php
+++ tp1/app/public/register.php
@@ -1,22 +1,23 @@
 <?php
 
-    require_once '../vendor/autoload.php';
+require_once '../vendor/autoload.php';
 
-    use App\Page;
-    
-    $page = new Page();
+use App\Page;
 
-    if(isset($_POST['send']))
-    {
-        $page->insert('users', [
-            'email' => $_POST['email'],
-            'nom' => $_POST['nom'],
-            'prenom' => $_POST['prenom'],
-            'adresse' => $_POST['adresse'],
-            'tel' => $_POST['tel'],
-            'password' => password_hash($_POST['password'], PASSWORD_DEFAULT)
-        ]);
-        header('Location: index.php');
-    }
-   
-    echo $page->render('register.html', []);
+$page = new Page();
+
+if(isset($_POST['send']))
+{
+    $page->insertUsers('users', [ // Utilisez insertUsers au lieu de insert
+        'email' => $_POST['email'],
+        'nom' => $_POST['nom'],
+        'prenom' => $_POST['prenom'],
+        'adresse' => $_POST['adresse'],
+        'tel' => $_POST['tel'],
+        'password' => password_hash($_POST['password'], PASSWORD_DEFAULT)
+    ]);
+    header('Location: index.php');
+    exit(); // Assurez-vous de terminer le script après la redirection
+}
+
+echo $page->render('register.html', []);
